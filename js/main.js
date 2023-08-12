388414c1f015aa67974ccb972f11afcbd4096329
Hayotillo <hayotnumon@gmail.com>
2023-08-12 16:25:47 +0500

    connect modal

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const showBtn = document.getElementById('show-btn')
 const modal = document.getElementById('modal')
+const closeModal = document.getElementById('close-modal')
 const overlay = document.getElementById('overlay')
 
 
@@ -9,6 +10,11 @@ showBtn.addEventListener('click', ()=>{
 })
 
 overlay.addEventListener('click', ()=>{
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+})
+
+closeModal.addEventListener('click', ()=>{
     modal.classList.add('hidden')
     overlay.classList.add('hidden')
 })
