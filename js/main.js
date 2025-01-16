980333507b8a38b3f6946b1b112deff19c3a6a31
Hayotillo <hayotnumon@gmail.com>
2025-01-16 07:37:59 +0500

    responsivlik yangilandi

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-const showBtn = document.getElementById('show-btn')
-const modal = document.getElementById('modal')
-const closeModal = document.getElementById('close-modal')
-const overlay = document.getElementById('overlay')
+// const showBtn = document.getElementById('show-btn')
+// const modal = document.getElementById('modal')
+// const closeModal = document.getElementById('close-modal')
+// const overlay = document.getElementById('overlay')
 
-showBtn.addEventListener('click', ()=>{
-    modal.classList.remove('hidden')
-    overlay.classList.remove('hidden')
-})
+// showBtn.addEventListener('click', ()=>{
+//     modal.classList.remove('hidden')
+//     overlay.classList.remove('hidden')
+// })
 
-overlay.addEventListener('click', ()=>{
-    modal.classList.add('hidden')
-    overlay.classList.add('hidden')
-})
+// overlay.addEventListener('click', ()=>{
+//     modal.classList.add('hidden')
+//     overlay.classList.add('hidden')
+// })
 
-closeModal.addEventListener('click', ()=>{
-    modal.classList.add('hidden')
-    overlay.classList.add('hidden')
-})
+// // closeModal.addEventListener('click', ()=>{
+// //     modal.classList.add('hidden')
+// //     overlay.classList.add('hidden')
+// // })
